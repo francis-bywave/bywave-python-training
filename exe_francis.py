63eacf3ad4654e19ee4b6baad9b5fbc005e7ab9a
EvilGelato <francis@bywave.com.au>
2018-10-11 18:07:41 +0800

    Exercise 1

---
+++ exe_francis.py
@@ -0,0 +1,14 @@
+print "Please input a number:"
+x = input()
+if x in range(60,101):
+ if x in range(60,75):
+  print "Derp!"
+ if x in range(75,85):
+  print "Good"
+ if x in range(85,95):
+  print "Very Good"
+ if x in range(95,101):
+  print "Level Asian!"
+else:
+ print "Invalid Value"
+
